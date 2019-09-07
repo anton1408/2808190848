c078f6dd395069c6a6a221583a14a319c7f8a419
Anton Luganskiy <luganskyi.anton@gmail.com>
2019-09-08 01:46:05 +0300

    finished styling messages

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="messages">
-    <div v-for="message in allMessages">
-      <p>{{ message.date}}</p>
-      <p>{{ message.userName}}</p>
-      <p>{{ message.message }}</p>
-      <hr>
+  <div class="chat-body">
+    <div class="chat-body__header">
+      <div class="chat-body__left-col">
+          <p>Помледние отзывы</p>
+          <p><a href="#">Все отзывы</a></p>
+      </div>
+      <div class="chat-body__right-col">
+        <div class="chat-body__quantity-likes"><p><span><img src="assets/img/heart-icon.png" alt=""></span>131</p></div>
+        <div class="chat-body__quantity-messages"><p><span><img src="assets/img/message-icon.png" alt=""></span>{{ allMessages.length }}</p></div>
+      </div>
     </div>
+
+    <div class="chat-body__messages" v-for="(message, index) in allMessages"
+                                    :key="index">
+      <div class="chat-body__message-header">
+        <p><span class="chat-body__user-name">{{ message.userName }}</span> <span class="chat-body__message-date">{{ message.date }}</span></p>
+      </div>
+      <div class="chat-body__message">
+        <p>{{ message.message }}</p>
+      </div>
+    </div>
+
     <send-message @adding-message="addMessage"></send-message>
   </div>
 
@@ -18,6 +33,7 @@
     },
     data() {
       return {
+        heartIcon: 'assets/img/heart-icon.png',
         allMessages: [
           {message: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!",
             date: "13 октября 2011",
@@ -31,7 +47,6 @@
                     "Особый вид куниц жизненно стабилизирует кинетический момент?",
             date: "14 октября 2011",
             userName: "Лилия Семёновна"}
-
         ]
       }
     },
@@ -44,5 +59,95 @@
 </script>
 
 <style lang="scss" scoped>
+  .chat-body__header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
 
+    .chat-body__left-col {
+      display: flex;
+      p:first-child {
+        font-size: 16px;
+        font-weight: bold;
+        margin-right: 5px;
+      }
+      p a {
+        font-size: 14px;
+        color: #005da1;
+      }
+    }
+    .chat-body__right-col {
+      display: flex;
+      .chat-body__quantity-likes {
+        margin-right: 18px;
+      }
+      .chat-body__quantity-messages {
+        margin-right: 2px;
+      }
+    }
+  }
+
+ .chat-body__quantity-likes,
+ .chat-body__quantity-messages {
+   p {
+     font-size: 12px;
+     line-height: 20px;
+   }
+ }
+  .chat-body__quantity-likes span,
+  .chat-body__quantity-messages span {
+    margin-right: 5px;
+  }
+  .chat-body__quantity-likes img,
+ .chat-body__quantity-messages img {
+   float: left;
+   position: relative;
+   top: 4px;
+ }
+ .chat-body__message-header {
+   margin-bottom: 11px;
+ }
+  .chat-body__user-name {
+    font-size: 14px;
+    font-weight: bold;
+  }
+  .chat-body__message-date {
+    font-size: 11px;
+    color: #808080;
+  }
+  .chat-body__message {
+    border: 1px solid #d1d2d2;
+    background-color: #f2fbff;
+    padding: 15px 15px 10px 15px;
+    margin-bottom: 17px;
+
+    p {
+      font-size: 14px;
+      line-height: 19px;
+    }
+  }
+  .chat-body__message::before,
+  .chat-body__message::after {
+    content: "";
+    width: 10px;
+    display: block;
+    position: absolute;
+
+  }
+  .chat-body__message::before {
+    background-color: #f2fbff;
+    border-left: 1px solid #d1d2d2;
+    border-top: 1px solid #d1d2d2;
+    height: 10px;
+    transform: rotate(45deg);
+    top: -6px;
+  }
+  .chat-body__message::after {
+    background-color: white;
+    border-right: 1px solid #d1d2d2;
+    border-bottom: 1px solid #d1d2d2;
+    height: 7px;
+    top: -8px;
+    left: 10px;
+  }
 </style>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -2,7 +2,7 @@
   <div class="services">
     <p class="services__title">Услуг</p>
     <div class="services__schedule">
-     <div class="services__left-column">
+     <div class="services__schedule-left-col">
        <div class="services__item">
          <p>Ручное бронирование</p>
        </div>
@@ -13,7 +13,7 @@
          <p>Отели</p>
        </div>
      </div>
-      <div class="services__right-column">
+      <div class="services__schedule-right-col">
         <div class="services__item">
           <p>11</p>
         </div>
@@ -24,8 +24,15 @@
           <p>1</p>
         </div>
       </div>
+    </div>
 
-
+    <div class="services__total">
+      <div class="services__total-left-col">
+        <p>Всего</p>
+      </div>
+      <div class="services__total-right-col">
+        <p>15</p>
+      </div>
     </div>
   </div>
 </template>
@@ -40,11 +47,12 @@
   .services {
     width: 320px;
     margin-left: auto;
+    margin-bottom: 22px;
   }
   .services__title {
     text-align: right;
     font-size: 13px;
-    padding-right: 28px;
+    padding-right: 35px;
     padding-bottom: 12px;
     line-height: 5px;
   }
@@ -55,16 +63,17 @@
     justify-content: space-between;
     padding-top: 15px;
     padding-bottom: 15px;
-    /*position: relative;*/
-    .services__right-column {
+    margin-bottom: 12px;
+
+    .services__schedule-right-col {
       width: 32%;
-      .services__item { p {text-align: center;} }
+      .services__item, { p {text-align: center;} }
     }
-    .services__left-column {
+    .services__schedule-left-col {
       width: 68%;
       margin-left: -1px;
     }
-    .services__left-column::before {
+    .services__schedule-left-col::before {
       background-color: rgba(83,83,83,0.4);
       content: '';
       width: 1px;
@@ -87,7 +96,7 @@
     }
   }
 
-  .services__left-column .services__item::before {
+  .services__schedule-left-col .services__item::before {
     content: "";
     border-radius: 0px 3px 3px 0px;
     position: absolute;
@@ -95,15 +104,43 @@
     background-color: #ace2f8;
     z-index: -10;
   }
-  .services__left-column .services__item:nth-child(1)::before {
+  .services__schedule-left-col .services__item:nth-child(1)::before {
     width: 218px;
     background-color: #b1e19b;
   }
-  .services__left-column .services__item:nth-child(2)::before {
+  .services__schedule-left-col .services__item:nth-child(2)::before {
     width: 53px;
   }
-  .services__left-column .services__item:nth-child(3)::before {
+  .services__schedule-left-col .services__item:nth-child(3)::before {
     width: 35px;
   }
+
+.services__total {
+  display: flex;
+  justify-content: space-between;
+
+  .services__total-left-col {
+    width: 68%;
+    margin-left: -1px;
+  }
+  .services__total-right-col {
+    width: 32%;
+  }
+
+  .services__total-left-col,
+  .services__total-right-col {
+    p {
+      font-size: 16px;
+      font-weight: bold;
+      line-height: 20px;
+    }
+  }
+  .services__total-right-col {
+    p {
+      text-align: center;
+    }
+  }
+
+}
 
 </style>
